cf4e8f7f39f6373ebd9d60adfeae54ff524d0524
arshidkv12 <arshidkv12@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-10 10:17:27 +0000

    svn update
    git-svn-id: https://plugins.svn.wordpress.org/wpgzip-compression/trunk@1217029 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wpgzip.php
@@ -0,0 +1,30 @@
+<?php 
+/* 
+Plugin Name: wpgzip Compression
+Plugin URI: http://seohref.com/wpgzip/
+Version: 1.1
+Description: This plugin reduce page weight by using gzip compression method.Gzip is the most popular and effective compression method. 
+Author: Arshid KV
+Author URI: http://seohref.com/
+*/
+ 
+/* Copyright 2015 Arshid  (email : arshidkv12@gmail.com)
+skype:arshidkv12
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version. */
+
+function wpgzip_comppression(){
+    if(substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip'))
+    {
+      ob_start("ob_gzhandler");
+    }else{
+        ob_start(); 
+    }
+}
+
+add_action("init","wpgzip_comppression");
+
+?>
